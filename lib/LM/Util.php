c16463fab7828cb4782515bf96ec40776e726e1e
zhangchunsheng <zhangchunsheng423@gmail.com>
2019-04-01 11:37:06 +0800

    add utf8Wordwrap

--- lib/LM/Util.php
+++ lib/LM/Util.php
@@ -79,6 +79,21 @@ class Util {
         return array('name' => '未知', 'en' => 'Unknown', 'short' => '', 'country' => 'CN');
     }
 
+    function utf8Wordwrap($string, $width = 75, $break = "\n", $cut = false) {
+        if($cut) {
+            // Match anything 1 to $width chars long followed by whitespace or EOS,
+            // otherwise match anything $width chars long
+            $search = '/(.{1,' . $width . '})(?:\s|$)|(.{' . $width . '})/uS';
+            $replace = '$1$2' . $break;
+        } else {
+            // Anchor the beginning of the pattern with a lookahead
+            // to avoid crazy backtracking when words are longer than $width
+            $search = '/(?=\s)(.{1,' . $width . '})(?:\s|$)/uS';
+            $replace = '$1' . $break;
+        }
+        return preg_replace($search, $replace, $string);
+    }
+
     /**
      * 获取 Redis 的实例
      *

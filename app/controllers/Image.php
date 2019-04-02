4346fe450880aedc10b2a6d0e2f748da0f302db5
zhangchunsheng <zhangchunsheng423@gmail.com>
2019-04-02 14:31:32 +0800

    add text to image

---
+++ app/controllers/Image.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peterzhang
+ * Date: 2019/4/2
+ * Time: 2:28 PM
+ */
+
+class ImageController extends ApplicationController {
+    // protected $layout = 'frontend';
+
+    public function indexAction() {
+
+    }
+}

a039ab78d44e731123717df3b2951824a7e8f2df
zhangchunsheng <zhangchunsheng423@gmail.com>
2019-04-02 17:51:48 +0800

    add text to image

--- lib/LM/ImageUtil.php
+++ lib/LM/ImageUtil.php
@@ -35,17 +35,17 @@ class ImageUtil {
         if(!empty($waterImage) && file_exists($waterImage)) {
             $isWaterImage = true;
             $waterInfo = getimagesize($waterImage);
-            $water_w = $waterInfo[0];//取得水印图片的宽
-            $water_h = $waterInfo[1];//取得水印图片的高
+            $waterW = $waterInfo[0];//取得水印图片的宽
+            $waterH = $waterInfo[1];//取得水印图片的高
             switch($waterInfo[2]) {//取得水印图片的格式
                 case 1:
-                    $water_im = imagecreatefromgif($waterImage);
+                    $waterImg = imagecreatefromgif($waterImage);
                     break;
                 case 2:
-                    $water_im = imagecreatefromjpeg($waterImage);
+                    $waterImg = imagecreatefromjpeg($waterImage);
                     break;
                 case 3:
-                    $water_im = imagecreatefrompng($waterImage);
+                    $waterImg = imagecreatefrompng($waterImage);
                     break;
                 default:
                     die($formatMsg);
@@ -55,8 +55,8 @@ class ImageUtil {
         //读取背景图片
         if(!empty($groundImage) && file_exists($groundImage)) {
             $groundInfo = getimagesize($groundImage);
-            $ground_w = $groundInfo[0];//取得背景图片的宽
-            $ground_h = $groundInfo[1];//取得背景图片的高
+            $groundW = $groundInfo[0];//取得背景图片的宽
+            $groundH = $groundInfo[1];//取得背景图片的高
             switch($groundInfo[2]) {//取得背景图片的格式
                 case 1:
                     $ground_im = imagecreatefromgif($groundImage);
@@ -76,75 +76,75 @@ class ImageUtil {
 
         //水印位置
         if($isWaterImage) {//图片水印
-            $w = $water_w;
-            $h = $water_h;
+            $w = $waterW;
+            $h = $waterH;
             $label = "图片的";
         } else {//文字水印
-            $temp = imagettfbbox(ceil($textFont * 5), 0, "./cour.ttf", $waterText);//取得使用 TrueType 字体的文本的范围
+            $temp = imagettfbbox(ceil($textFont * 5), 0, PS_ROOT . "/fonts/SimHei.ttf", $waterText);//取得使用 TrueType 字体的文本的范围
             $w = $temp[2] - $temp[6];
             $h = $temp[3] - $temp[7];
             unset($temp);
             $label = "文字区域";
         }
 
-        if(($ground_w < $w) || ($ground_h < $h)) {
+        if(($groundW < $w) || ($groundH < $h)) {
             echo "需要加水印的图片的长度或宽度比水印" . $label . "还小，无法生成水印！";
             return;
         }
 
         switch($waterPos) {
             case 0://随机
-                $posX = rand(0,($ground_w - $w));
-                $posY = rand(0,($ground_h - $h));
+                $posX = rand(0,($groundW - $w));
+                $posY = rand(0,($groundH - $h));
                 break;
             case 1://1为顶端居左
                 $posX = 0;
                 $posY = 0;
                 break;
             case 2://2为顶端居中
-                $posX = ($ground_w - $w) / 2;
+                $posX = ($groundW - $w) / 2;
                 $posY = 0;
                 break;
             case 3://3为顶端居右
-                $posX = $ground_w - $w;
+                $posX = $groundW - $w;
                 $posY = 0;
                 break;
             case 4://4为中部居左
                 $posX = 0;
-                $posY = ($ground_h - $h) / 2;
+                $posY = ($groundH - $h) / 2;
                 break;
             case 5://5为中部居中
-                $posX = ($ground_w - $w) / 2;
-                $posY = ($ground_h - $h) / 2;
+                $posX = ($groundW - $w) / 2;
+                $posY = ($groundH - $h) / 2;
                 break;
             case 6://6为中部居右
-                $posX = $ground_w - $w;
-                $posY = ($ground_h - $h) / 2;
+                $posX = $groundW - $w;
+                $posY = ($groundH - $h) / 2;
                 break;
             case 7://7为底端居左
                 $posX = 0;
-                $posY = $ground_h - $h;
+                $posY = $groundH - $h;
                 break;
             case 8://8为底端居中
-                $posX = ($ground_w - $w) / 2;
-                $posY = $ground_h - $h;
+                $posX = ($groundW - $w) / 2;
+                $posY = $groundH - $h;
                 break;
             case 9://9为底端居右
-                $posX = $ground_w - $w - 10;   // -10 是距离右侧10px 可以自己调节
-                $posY = $ground_h - $h - 10;   // -10 是距离底部10px 可以自己调节
+                $posX = $groundW - $w - 10;   // -10 是距离右侧10px 可以自己调节
+                $posY = $groundH - $h - 10;   // -10 是距离底部10px 可以自己调节
                 break;
             default://随机
-                $posX = rand(0,($ground_w - $w));
-                $posY = rand(0,($ground_h - $h));
+                $posX = rand(0,($groundW - $w));
+                $posY = rand(0,($groundH - $h));
                 break;
         }
 
         //设定图像的混色模式
         imagealphablending($ground_im, true);
         if($isWaterImage) {//图片水印
-            imagecopy($ground_im, $water_im, $posX, $posY, 0, 0, $water_w, $water_h);//拷贝水印到目标文件
+            imagecopy($ground_im, $waterImg, $posX, $posY, 0, 0, $waterW, $waterH);//拷贝水印到目标文件
         } else {//文字水印
-            if(!emptyempty($textColor) && (strlen($textColor)==7)) {
+            if(!empty($textColor) && (strlen($textColor)==7)) {
                 $R = hexdec(substr($textColor,1,2));
                 $G = hexdec(substr($textColor,3,2));
                 $B = hexdec(substr($textColor,5));
